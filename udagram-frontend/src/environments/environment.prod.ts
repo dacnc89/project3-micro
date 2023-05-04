10423c967c8c2ab9b2244f7473671e7cf091b58a
zimean <phan.van.diep@smartcamp.co.jp>
2023-05-04 13:08:53 +0700

    update url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab2a5e518b53f4da580a007c200ad597-1191724112.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
